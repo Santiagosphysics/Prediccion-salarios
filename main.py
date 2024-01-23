84edce99c7106509a06f36034c11008e6be1cf24
Santielpapi <msjimenez@unbosque.edu.co>
2024-01-22 21:05:50 -0500

    1.6

--- main.py
+++ main.py
@@ -1,5 +1,28 @@
-from utils import preprocessing
+from utils import pre
 
-data = preprocessing().creation_data('Salary_Data.csv')
-target, features = preprocessing().declaration_var(data, 'Salary')
-print(data.info())
+data = pre().creation_data('Salary_Data.csv')
+target, features, names_f = pre().declaration_var(data, 'Salary')
+
+
+one_gender, features = pre().one_hot('Gender', features)
+one_education, features = pre().one_hot('Education Level', features)
+
+features["Education Level_Bachelor's"], features = pre().replace("Education Level_Bachelor's", "Education Level_Bachelor's Degree", features )
+features["Education Level_Master's"], features = pre().replace("Education Level_Master's", "Education Level_Master's Degree", features )
+features["Education Level_PhD"], features = pre().replace("Education Level_PhD", "Education Level_phD", features )
+
+features['Age'] = features['Age'].astype('int16')
+features['Years of Experience'] = features['Years of Experience'].astype('int64')
+
+
+
+one_job, features = pre().one_hot('Job Title', features)
+
+
+print(target.info())
+
+
+# print(features['Education Level_Masters Degree'])
+# print(one_education.sum())
+
+# print(one_gender)
--- utils.py
+++ utils.py
@@ -1,36 +1,47 @@
 import pandas as pd 
-# import sklearn.
+import numpy as np 
+from sklearn.preprocessing import OneHotEncoder
 
-class preprocessing:
+class pre:
     def creation_data(self, path_csv):
         df = pd.read_csv(path_csv)
         return df
     
     def declaration_var(self, data, target):
+        
         target_1 = data[target]
         features = data.drop(target, axis=1)
         names_f = {name:set() for name in features}
-        for i in features:
-            for j in features[i]:
-                names_f[i].add(j)
+                
         for i in features:
             index_drop = features[i][features[i].isnull() == True].index
             features.drop(index_drop, inplace = True)
+            
+        for i in features:
+            for j in features[i]:
+                names_f[i].add(j)        
+        return target_1, features, names_f
+    
+    def one_hot(self, variable, features):
+        encoder = OneHotEncoder()
+        feature_encoded = encoder.fit_transform(features[[variable]])
+        df_one_hot = pd.DataFrame(feature_encoded.toarray(), columns = encoder.get_feature_names_out([variable]))
         
-        return target_1, features
-    
+        features.drop(variable, axis = 1, inplace=True)        
+        names_one = [name for name in df_one_hot]
+        df_one_hot[names_one] = df_one_hot[names_one].astype('int8')
+        features.reset_index(drop=True, inplace=True)
+        df_one_hot.reset_index(drop=True, inplace=True)
+        features = pd.concat([features, df_one_hot], axis = 1)
+        return df_one_hot, features
 
-    
-    
-data = preprocessing().creation_data('Salary_Data.csv')
-target, features = preprocessing().declaration_var(data, 'Salary')
-# print(data.info())
+    def replace(self, var_1, var_2, features):
+        features[var_1] = features[var_1] + features[var_2]
+        features.drop(var_2, axis =1, inplace=True)
+        return features[var_1], features
 
 
 
+data = pre().creation_data('Salary_Data.csv')
+target, features, names_f = pre().declaration_var(data, 'Salary')
 
-
-
-print(features.isnull().sum())
-print(len(features['Age']))
-# print(data.info())
