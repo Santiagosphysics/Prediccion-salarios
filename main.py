27698928e9f3e8fa4f5ca7b1af26a2bad9a59c70
Santielpapi <msjimenez@unbosque.edu.co>
2024-01-27 13:21:55 -0500

    1.17

--- main.py
+++ main.py
@@ -24,26 +24,8 @@ cat_features = ['Gender', 'Education Level', 'Job Title']
 #Convertir las variables catégoricas en OneHotEncoder
 num_transformer, cat_transformer = pipe(cat_features, features)
 preprocessor = column_trans(num_transformer, num_features, cat_transformer, cat_features)
-
 X_train, X_test,y_train, y_test = train_test_split(features, target, test_size=0.2, random_state=42)
-
 models = gen_pipe(preprocessor)
-
 models_metrics = pre().df_metrics(models, X_train, X_test, y_train, y_test)
-# models_metrics = {i:[] for i in range(len(models)) }
 
-# for i in models_metrics:
-#     model = models[i].fit(X_train, y_train)
-#     prediction = model.predict(X_test)
-#     r2 = r2_score(y_test, prediction)
-#     mse = mean_squared_error(y_test, prediction)
-#     rmse = np.sqrt(mean_squared_error(y_test, prediction))
-#     list_1 = [r2,mse,rmse]
-#     models_metrics[i] = [round(metrics, 2) for metrics in list_1]
-
-# names = ['LinearRegression', 'Ridge', 'RandomForestRegressor']
-# index_final = ['R2', 'mse', 'rmse']
-
-# models_metrics = pd.DataFrame(data = models_metrics,index = index_final)
-# models_metrics.columns = names
 print(models_metrics)
